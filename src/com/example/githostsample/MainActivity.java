d3455a4c2214c527fc70a4a20b8d1b49ddcded04
rameshg8b <ramesh.bugatha@weboniselab.com>
2013-01-25 13:20:04 +0530

    comment modified to 3

--- src/com/example/githostsample/MainActivity.java
+++ src/com/example/githostsample/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-    	// Test comment
+    	// Test comment 3
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
