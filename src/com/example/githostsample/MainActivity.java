d2f47992e9f27f979bbf65c8f4e58099361c3cb3
rameshg8b <ramesh.bugatha@weboniselab.com>
2013-01-28 11:46:48 +0530

    added one dummy method

--- src/com/example/githostsample/MainActivity.java
+++ src/com/example/githostsample/MainActivity.java
@@ -19,5 +19,9 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
+    public void test()
+    {
+    int x;
+    }
     
 }
